3aebbda50d0ba95791930c42fd2590e8b0473eaf
tumrod <tipparat.umrod@utexas.edu>
2015-05-02 05:17:08 -0500

    add readme and modify scripts for universal usage

--- README.txt
+++ README.txt
@@ -1,7 +1,7 @@
 # bisque-lungMap
 
 Requirements:
-python
+python 2.7
 virtualenv - $ easy_install virtualenv
 pip - $ pip sudo apt-get install python-pip
 BeautifulSoup4 - $ easy_install BeautifulSoup4
@@ -11,11 +11,21 @@ Clone:
 git clone https://github.com/tumrod/bisque-lungMap.git
 =============================================================
 
-1.  create a virtualenv and fetch bqapi with the CAS option
-$ virtualenv bqtest
+1. Create a virtualenv and fetch bqapi with the CAS option
+$ virtualenv bqapi
+$ source bqapi/bin/activate 
+$ pip install -i http://biodev.ece.ucsb.edu/py/bisque/dev bqapi[CAS]
 
-2. activate the environment
-$ source bqtest/bin/activate 
+2. Move necessary packages into site-packages
+(bqapi) $ sh setup_src.sh
 
-3. Run the script 
-(bqtest) $ python <script_file.py>
+3. To run a python script, make sure to activate the virtualenv 
+$ source bqapi/bin/activate 
+
+4. Run the script file
+(bqapi) $ python <script_file.py>
+
+** You can deactivate the virtualenv by $ deactivate 
+
+==============================================================
+
---
+++ Scripts/get_list.py
@@ -0,0 +1,30 @@
+from readInFile_r import readInFile_r
+
+##############################################################################
+## Generating a list of annotations from file                               ##
+## The file must be tab-delimited txt file                                  ##
+##                                                                          ##
+##############################################################################
+def getList():
+    totalList = []
+    annotationList = []
+    
+    # open file
+    fileName = raw_input("Enter annotation file location: ")
+    totalList = readInFile_r(fileName)
+
+    return totalList
+
+###############################################################################
+## getFileName() parameters                                                  ##
+##               - theList, can be from getList() or list of filenames,      ##
+##               which has matched names with filenames on bisque images     ##
+##               - index, the index of the names or filenames                ##
+##               - word, additional string,                                  ##
+##               which can be the file extension, for example, ".jpg"        ##
+###############################################################################
+def getFileName(theList,index, word):
+    names = []
+    for i in range(len(theList)):
+        names.append(theList[i][index]+word)
+    return names
